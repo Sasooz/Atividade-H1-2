d03c22a5e0901873db6e30b962dbb91126f51e92
savio.cardoso <savio.cardoso@ZE-TI79>
2024-08-16 16:55:19 -0300

    Adicionar arquivos de projeto.

---
+++ Classes/CalcularFrete.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Atividade_H1_2.Classes;
+
+public class CalcularFrete: CalculoFrete
+{
+    
+
+    public double CalculoFrete(CalculoFrete frete)
+    {
+        int TaxaEstado;
+        if (frete.UF == "SP")
+        {
+            TaxaEstado = 50;
+        }
+        else if (frete.UF == "RJ")
+        {
+            TaxaEstado = 60;
+        }
+        else if (frete.UF == "MG")
+        {
+            TaxaEstado = 55;
+        }
+        else TaxaEstado = 70;
+
+        var VolumeProduto = frete.AlturaProduto * frete.LarguraProduto * frete.ComprimentoProduto;
+        frete.ValorFrete = VolumeProduto * 0.02 + TaxaEstado;
+
+        return frete.ValorFrete;
+    }
+}
+
---
+++ Controllers/FreteController.cs
@@ -0,0 +1,19 @@
+﻿using Atividade_H1_2.Classes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atividade_H1_2.Controllers;
+
+[ApiController]
+[Route("frete")]
+public class FreteController : ControllerBase
+{
+  
+    [HttpPost]
+    public IActionResult CalcularFreteTodo(CalculoFrete frete)
+    {
+        CalcularFrete _calcularFrete = new CalcularFrete();
+        var resultado = _calcularFrete.CalculoFrete(frete);
+
+        return Ok(resultado);
+    }
+}
